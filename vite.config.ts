d84c4a55ee8f8b60cd042126a786a7ded57cb78b
DimaDevelopment <dima.development@wandry.com.ua>
2024-04-10 11:34:31 +0300

    fixed distDir option

--- vite.config.ts
+++ vite.config.ts
@@ -29,7 +29,7 @@ export default defineConfig({
     target: process.env.TAURI_PLATFORM == "windows" ? "chrome105" : "safari13",
     minify: !process.env.TAURI_DEBUG ? "esbuild" : false,
     sourcemap: !!process.env.TAURI_DEBUG,
-    outDir: "../dist",
+    outDir: "./dist",
   },
   plugins: [vue()],
 });
